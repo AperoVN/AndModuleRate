a2bf2a687e6e8910297ab96ec6f3e71f2d103ef4
datnt28 <datnt@apero.vn>
2023-04-27 20:55:08 +0700

    create rating script dialog

--- app/src/main/java/com/example/andmodulerate/MainActivity.java
+++ app/src/main/java/com/example/andmodulerate/MainActivity.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.rate.control.CallbackListener;
 import com.rate.control.OnCallback;
+import com.rate.control.RatingScriptListener;
+import com.rate.control.dialog.RatingScriptDialog;
 import com.rate.control.funtion.RateUtils;
 
 public class MainActivity extends AppCompatActivity {
@@ -31,16 +34,41 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        RateUtils.showRateEmojiDialog(this, new CallbackListener() {
-            @Override
-            public void onMaybeLater() {
-                finish();
-            }
+//        RateUtils.showRateEmojiDialog(this, new CallbackListener() {
+//            @Override
+//            public void onMaybeLater() {
+//                finish();
+//            }
+//
+//            @Override
+//            public void onRating(float rating, String feedback) {
+//                runOnUiThread(() -> Toast.makeText(MainActivity.this, "rate: " + rating + " feedback: " + feedback, Toast.LENGTH_SHORT).show());
+//            }
+//        });
 
-            @Override
-            public void onRating(float rating, String feedback) {
-                runOnUiThread(() -> Toast.makeText(MainActivity.this, "rate: " + rating + " feedback: " + feedback, Toast.LENGTH_SHORT).show());
-            }
-        });
+        RateUtils.showRatingScript(
+                this,
+                new RatingScriptListener() {
+                    @Override
+                    public void onComplete(@NonNull com.google.android.gms.tasks.Task<com.google.android.play.core.review.ReviewInfo> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getBaseContext(), getString(R.string.thanks_feedback), Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(getBaseContext(), "Rate error", Toast.LENGTH_LONG).show();
+                        }
+                        MainActivity.super.onBackPressed();
+                    }
+                    @Override
+                    public void onResultRated(int ratedLevel) {
+                        Toast.makeText(getBaseContext(), "Rated "+ratedLevel+" star", Toast.LENGTH_LONG).show();
+                        MainActivity.super.onBackPressed();
+                    }
+                    @Override
+                    public void onExit() {
+                        MainActivity.super.onBackPressed();
+                    }
+                },
+                getString(R.string.app_name)
+        );
     }
 }
